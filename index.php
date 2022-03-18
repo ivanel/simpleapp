f0ec37690f591291e0ea0cb0939ece65f3f01b1f
ivanel <hp.ivan@gmail.com>
2022-03-18 16:18:49 -0600

    index.php file modified

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 	<h1>PHP Test</h1>
 		<p><b>An Example of PHP in Action</b></p>
 		<p><b>This is an example line added by ivanel</b></p>
+		<p><b>This is an example second line added by ivanel</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information</h2> 
