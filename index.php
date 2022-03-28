6bce37ed17a6ba1f8cfd652c888dc4a4d9c4e6ad
ivanel <hp.ivan@gmail.com>
2022-03-27 21:40:10 -0600

    index content fixed

--- index.php
+++ index.php
@@ -7,8 +7,6 @@
 <body> 
 	<h1>PHP Test</h1>
 		<p><b>An Example of PHP in Action</b></p>
-		<p><b>This is an example line added by ivanel</b></p>
-		<p><b>This is an example second line added by ivanel</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information</h2> 
