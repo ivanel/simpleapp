89fbeacb1af1c11a4b8b6365a2d16f12704f232b
ivanel <hp.ivan@gmail.com>
2022-04-06 19:57:48 -0500

    Index content modified

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 <body> 
 	<h1>PHP Test</h1>
 		<p><b>An Example of PHP in Action from lab119-1 branch</b></p>
+		<p><b>another line from lab119-1 branch</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information</h2> 
